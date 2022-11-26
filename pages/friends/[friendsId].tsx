dec3854e8e4c0fc650aaa6563c6384ef26d83b5a
KONG1034 <kyh93054@naver.com>
2022-11-26 10:42:49 +0900

    Kong - 메타 태그 추가

--- pages/friends/[friendsId].tsx
+++ pages/friends/[friendsId].tsx
@@ -2,10 +2,18 @@ import FriendInfo from "../../Layout/FriendInfo";
 import FriendProfile from "../../Layout/FriendProfile";
 import Header from "../../Layout/Header";
 import classes from '../../styles/home.module.scss';
+import Head from 'next/head';
 
 
 export default function FriendId() {
     return <>
+    <Head>
+        <title>Friends List</title>
+        <meta
+            name="Friends List"
+            content="Get Your friends list"
+        />
+    </Head>
         <section className={classes.container}>
         <Header/>
         <FriendProfile/>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,17 @@ import classes from '../styles/home.module.scss';
 import Friends from '../Layout/Friends';
 import Header from '../Layout/Header';
 import MyProfile from '../Layout/MyProfile';
+import Head from 'next/head';
 
 export default function Home() {
   return <>
+  <Head>
+    <title>PhoneBook</title>
+    <meta 
+      name="description"
+      content="add, delete, update your friends in the PhoneBook"
+    />
+  </Head>
     <section className={classes.container}>
       <Header/>
       <MyProfile/>
