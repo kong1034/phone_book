eec2257ce1c239592bc1b5991bcac599fe07c187
KONG1034 <kyh93054@naver.com>
2022-12-03 21:35:51 +0900

    Kong - 헤더 및 친구 목록 css 수정 및 뒤로가기 버튼 구현

--- Layout/Header.tsx
+++ Layout/Header.tsx
@@ -1,9 +1,29 @@
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import classes from '../styles/header.module.scss';
 
 export default function Header() {
+    let check = false;
+    const router = useRouter();
+    const goBack = () => {
+        router.back();
+    }
+    router.pathname === "/" ? check = true : check = false;
+    console.log(check);
     return <>
         <header className={classes.header}>
+            <div>
+                <div onClick={goBack}>
+                    {
+                        check === true ? "" : "<"
+                    }
+                </div>
+                {/* <img onClick={goBack} src="https://w7.pngwing.com/pngs/50/539/png-transparent-arrow-computer-icons-white-angle-white-text-thumbnail.png" alt="뒤로가기"/> */}
+            </div>
             <h1>Phone Book</h1>
+            <div>
+                <div>+</div>
+            </div>
         </header>
     </>
 }
